21a451cb2ab4b3c0cbc0951697a2bcb464783641
Christian Martinez <christian@mixdlabs.com>
2023-05-13 10:31:57 -0600

    Added html

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   {
-    $('.save-btn').click(function () {})
+    $('.saveBtn').click(function (e) {
+      console.log(e)
+    })
   }
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
